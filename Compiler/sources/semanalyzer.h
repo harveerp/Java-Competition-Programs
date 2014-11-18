b83393a69ddc3e51ed8b1eaaa607cc56e7c9868f
harveerp <harveerp@gmail.com>
2014-11-18 11:44:49 -0800

    Folder in Wrong Place
    Compiler Folder should not be there

--- Compiler/sources/semanalyzer.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SEMANALYZER_H_
-#define _SEMANALYZER_H_
-
-void do_semantic_analysis();
-
-#endif
--- Compiler/sources/tracer.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TRACER_H_
-void parse_trace( int, int );
-void tracer_init( int, int );
-
-#endif
-
